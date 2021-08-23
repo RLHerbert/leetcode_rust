431f2458106fa8db02c5272f4daf61d96f84f11f
Rowan Herbert <rlherbert.dev@gmail.com>
2021-08-22 17:40:36 -0700

    Cleaned up easy problems and introduced tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
-mod easy;
+// mod easy;
 mod hard;
+mod solutions;
 
 #[cfg(test)]
 mod tests {
+    use super::solutions::Solution;
     #[test]
-    fn it_works() {
-        // assert_eq!(2 + 2, 4);
+    #[ignore]
+    fn p1_two_sum() {
+        let (nums, target) = (vec![2, 7, 11, 15], 9);
+        assert_eq!(Solution::two_sum(nums, target), vec![0, 1]);
+
+        let (nums, target) = (vec![3, 2, 4], 6);
+        assert_eq!(Solution::two_sum(nums, target), vec![1, 2]);
+
+        let (nums, target) = (vec![3, 3], 6);
+        assert_eq!(Solution::two_sum(nums, target), vec![0, 1]);
+    }
+
+    #[test]
+    #[ignore]
+    fn p1920_array_from_perm() {
+        let nums = vec![0, 2, 1, 5, 3, 4];
+        assert_eq!(Solution::build_array(nums), vec![0, 1, 2, 4, 5, 3]);
+
+        let nums = vec![5, 0, 1, 2, 3, 4];
+        assert_eq!(Solution::build_array(nums), vec![4, 5, 0, 1, 2, 3]);
     }
 }
---
+++ src/solutions.rs
@@ -0,0 +1,6 @@
+pub mod easy;
+pub mod hard;
+pub mod medium;
+
+#[allow(dead_code)]
+pub struct Solution;
